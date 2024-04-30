37a7321dd34d89f8ef899554538aecdb30d6ea5c
priyaK9157 <priyakumari2003004@gmail.com>
2024-04-30 11:55:03 +0530

    first commit

---
+++ server/controller/DecodeToken.js
@@ -0,0 +1,33 @@
+const jwt = require("jsonwebtoken");
+
+exports.DecodeToken = async (req, res) => {
+    console.log("hiii")
+    try {
+        const { token } = req.body;
+        try {
+            const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+            console.log("decodedToken", decodedToken)
+            return res.json({
+                Email: decodedToken.email
+            });
+        } catch (error) {
+            // Handle invalid signature error
+            if (error.name === 'JsonWebTokenError' && error.message === 'invalid signature') {
+                return res.json({
+                    response: "Invalid Signature"
+                });
+            } else {
+                // Handle other errors
+                console.log("error", error);
+                return res.status(500).json({
+                    response: "Internal Server Error"
+                });
+            }
+        }
+    } catch (error) {
+        console.log("error", error);
+        return res.status(500).json({
+            response: "Internal Server Error"
+        });
+    }
+};
